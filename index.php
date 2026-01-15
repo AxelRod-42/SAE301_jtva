cba99db8b063c90166a08dd12fc84302b11ef020
julian-loze <julian.loze@etu.uca.fr>
2026-01-15 14:42:53 +0100

    htlm dans php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
 require_once "Team.php";
 
@@ -6,3 +16,7 @@ $team = new Team();
 $teams = $team->getAll();
 
 print_r($teams);
+?>
+
+</body>
+</html>
