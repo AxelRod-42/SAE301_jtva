b123a23af2d3270708599eaaf5e5f2ddc69c20c9
AxelRod-42 <Axel.Rodriguez@etu.uca.fr>
2026-01-16 19:45:08 +0100

    Header Page d'accueil

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Saint-Médard Basket</title>
+    <link rel="stylesheet" href="CSS/style.css"> <!--Lien vers le css-->
+    <link rel="icon" type="image/x-icon" href="image/logo/favicon.ico"><!--icone du site-->
+</head>
+
+<body>
+    <header>
+        <section class="top">
+            <nav class="menu">
+                <ul>
+                    <li><a href="pages/club.php">Club</a></li>
+                    <li><a href="pages/boutique.php">Boutique</a></li>
+                    <li><a href="pages/calendrier.php">Calendrier</a></li>
+                    <div class="logo">
+                        <a href="index.php"><img src="image/logo/logo_StMedard.png" alt="Logo Saint-Médard Basket" width="250" height="200"></a>
+                    </div>
+                    <li><a href="pages/actualites.php">Actualités</a></li>
+                    <li><a href="pages/contact.php">Contact</a></li>
+                    <div class="panier">
+                        <a href="pages/boutique.php"><img src="image/panier.png" alt="Panier" width="100" height="100"></a>
+                    </div>
+                </ul>
+            </nav>
+        </section>
+    </header>
+
