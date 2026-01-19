d90351e0f8131009f9fed10ce201746d57ebd900
julian-loze <julian.loze@etu.uca.fr>
2026-01-19 09:43:08 +0100

    event fini

--- event.php
+++ event.php
@@ -2,15 +2,8 @@
 
 require_once __DIR__ . "/database.php";
 
-
 class Event
 {
-    private int $id;
-    private string $nom_event;
-    private string $date_event;
-    private string $description;
-    private string $type;
-
     private PDO $db;
 
     public function __construct()
@@ -18,64 +11,14 @@ class Event
         $this->db = Database::getConnection();
     }
 
-    /* =====================
-       Méthodes métier
-    ====================== */
-
-    public function getUpcoming(): array
+    public function getAllEvents(): array
     {
-        $sql = "SELECT * FROM events WHERE date_event >= CURDATE()";
+        $sql = "
+            SELECT *
+            FROM evenements
+            ORDER BY debut ASC
+        ";
+
         return $this->db->query($sql)->fetchAll();
-    }
-
-    /* =====================
-       Getters / Setters
-    ====================== */
-
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    // Pas de setter pour l'id
-
-    public function getNomEvent(): string
-    {
-        return $this->nom_event;
-    }
-
-    public function setNomEvent(string $nom_event): void
-    {
-        $this->nom_event = $nom_event;
-    }
-
-    public function getDateEvent(): string
-    {
-        return $this->date_event;
-    }
-
-    public function setDateEvent(string $date_event): void
-    {
-        $this->date_event = $date_event;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): void
-    {
-        $this->description = $description;
-    }
-
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    public function setType(string $type): void
-    {
-        $this->type = $type;
     }
 }
